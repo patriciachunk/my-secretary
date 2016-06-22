4c452692cac8a256a624d62d286f3eca0855b51a
Patricia Chun <yejeechun@gmail.com>
2016-06-22 13:30:02 -0400

    Basic setup of AuthControlller.

---
+++ app/auth/auth.controller.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mutantApp.auth')
+    .controller('AthController', AuthController);
+
+    AuthController.$inject = [];
+
+    fucntion AuthController() {
+      var vm = this;
+    }
+})
