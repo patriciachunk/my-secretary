afc9cead7b6efdf3196f21a2994d0d0c4b14dc27
Patricia Chun <yejeechun@gmail.com>
2016-06-27 14:08:46 -0400

    Add run function and have it listen for stateChangeErrors on . If error is 'AUTH_REQUIRED', redirect to login.

--- app/app.module.js
+++ app/app.module.js
@@ -14,11 +14,21 @@
     'mutantApp.core',
     'mutantApp.layout',
   ])
-   .config(configFunction);
+  .config(configFunction)
+  .run(runFunction);
 
    configFunction.$inject = ['$urlRouterProvider'];
 
    function configFunction($urlRouterProvider) {
      $urlRouterProvider.otherwise('/');
+   }
+   runFunction.$inject = ['$rootScope', '$state'];
+
+   function runFunction($rootScope, $state) {
+     $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+       if (error === "AUTH_REQUIRED") {
+         $state.go('login');
+       }
+     });
    }
 })();
