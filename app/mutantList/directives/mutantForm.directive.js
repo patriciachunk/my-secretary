3577916d4f62c3b020e0ac08c9d2bd97565867a1
Patricia Chun <yejeechun@gmail.com>
2016-06-28 14:44:31 -0400

    Add MutantFormController to directive. Add isolate scope and pass vm.mutants into it through mutants property. Add addMutant and newMutant to controller.

--- app/mutantList/directives/mutantForm.directive.js
+++ app/mutantList/directives/mutantForm.directive.js
@@ -9,7 +9,26 @@
     return {
       templateUrl: 'app/mutantList/directive/mutantForm.html',
       restrict: 'E',
-      
-    }
+      controller: MutantFormController,
+      controllerAs: 'vm',
+      bindTocontroller: true,
+      scope: {
+        mutants: '=',
+      },
+    };
   }
-})
+
+  MutantFormController.$inject = ['mutantService'];
+
+  function MutantFormController(mutantService) {
+    var vm = this;
+
+    vm.addMutant = addMutant;
+    vm.newMutant = new MutantService.Mutant();
+
+    function addMutant() {
+       vm.mutants.$add(vm.newMutant);
+       vm.newMutant = new mutantService.Mutant();
+     }
+  }
+})();
